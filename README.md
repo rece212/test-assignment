4a7421739cea803f5127ffe3debed9859a5fa736
rece212 <rwanvig@iie.ac.za>
2022-05-11 13:35:16 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# prog6221-task-1-hand-in-rece212
+prog6221-task-1-hand-in-rece212 created by GitHub Classroom
+
+
+
+Reece Wanvig assigment part 1 - ST56789
+Youtube link : www.youtube.com
---
+++ Test assignment hand in/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test_assignment_hand_in
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello gabe");
+        }
+    }
+}
